2ca2cd84eb72f3dd830db63bba7a1a6ff4676028
mees <meeslursen@gmail.com>
2024-05-17 12:42:43 +0200

    fix and added testDFA and testNFA

--- zz_haskellCode/DfaAndNfaCode.hs
+++ zz_haskellCode/DfaAndNfaCode.hs
@@ -1,15 +1,26 @@
 module DfaAndNfaCode where
 
-data DFA a b = DFA  { statesDFA :: [a]
-                    , alphabetDFA :: [b]
-                    , transitionDFA :: (a,b) -> a
-                    , beginDFA :: a
-                    , finalDFA :: [a]
+import Data.Function 
+import Data.List 
+import Data.Maybe
+
+
+data DFA state symbol = DFA  
+                    { statesDFA :: [state]
+                    , alphabetDFA :: [symbol]
+                    , transitionDFA :: (state,symbol) -> state
+                    , beginDFA :: state
+                    , finalDFA :: [state]
                     }
 
-data NFA a b = NFA  { statesNFA :: [a]
-                    , alphabetNFA :: [b]
-                    , transitionNFA :: (a,b) -> [a]
-                    , beginNFA :: a
-                    , finalNFA :: [a]
+
+data NFA state symbol = NFA  
+                    { statesNFA :: [state]
+                    , alphabetNFA :: [symbol]
+                    , transitionNFA :: (state,symbol) -> [state]
+                    , beginNFA :: state
+                    , finalFNA :: [state]
                     }
+
+testDFA = DFA [1,2] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1,'a'), 1), ((1,'b'), 2)])  1 [2]
+testNFA = NFA [1,2,3] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1,'a'), [1]), ((1,'b'), [1,2]), ((2,'a'), [2]), ((2,'b'), [2])])  1 [2]
