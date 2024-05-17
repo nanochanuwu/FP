23271c2151f414d86632419df6c9048dcf8aefcd
mees <meeslursen@gmail.com>
2024-05-17 10:53:24 +0200

    minor fix data types

--- zz_haskellCode/DfaAndNfaCode.hs
+++ zz_haskellCode/DfaAndNfaCode.hs
@@ -1,15 +1,15 @@
 module DfaAndNfaCode where
 
-data DFA a b    { statesDFA :: [a]
-                , alphabetDFA :: [b]
-                , transitionDFA :: (a,b) -> a
-                , beginDFA :: a
-                , finalDFA :: a
+data DFA a b = DFA  { statesDFA :: [a]
+                    , alphabetDFA :: [b]
+                    , transitionDFA :: (a,b) -> a
+                    , beginDFA :: a
+                    , finalDFA :: a
                 }
 
-data NFA a b    { statesNFA :: [a]
-                , alphabetNFA :: [b]
-                , transitionNFA :: (a,b) -> [a]
-                , beginNFA :: a
-                , finalFNA :: a
-                }
+data NFA a b = NFA  { statesNFA :: [a]
+                    , alphabetNFA :: [b]
+                    , transitionNFA :: (a,b) -> [a]
+                    , beginNFA :: a
+                    , finalFNA :: a
+                    }
