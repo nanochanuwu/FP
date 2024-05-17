95f87c35aa88f36d638b1de55dea2096349d4501
mees <meeslursen@gmail.com>
2024-05-17 14:54:52 +0200

    Added evaluateDFA function which takes a list of symbols and checks whether the walk along the DFA from the start states ends in some final state. Also changed the data type for transitionNFA :: (state, Maybe symbol) -> [state]. It now takes Maybe symbol to be able to include Nothing as a denotion of epsilon.

--- zz_haskellCode/DfaAndNfaCode.hs
+++ zz_haskellCode/DfaAndNfaCode.hs
@@ -1,11 +1,11 @@
 module DfaAndNfaCode where
 
-import Data.Function 
-import Data.List 
+import Data.Function
+import Data.List
 import Data.Maybe
 
 
-data DFA state symbol = DFA  
+data DFA state symbol = DFA
                     { statesDFA :: [state]
                     , alphabetDFA :: [symbol]
                     , transitionDFA :: (state,symbol) -> state
@@ -14,13 +14,26 @@ data DFA state symbol = DFA
                     }
 
 
-data NFA state symbol = NFA  
+
+
+data NFA state symbol = NFA
                     { statesNFA :: [state]
                     , alphabetNFA :: [symbol]
-                    , transitionNFA :: (state,symbol) -> [state]
+                    , transitionNFA :: (state, Maybe symbol) -> [state]
                     , beginNFA :: state
                     , finalFNA :: [state]
                     }
 
+
 testDFA = DFA [1,2] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1,'a'), 1), ((1,'b'), 2)])  1 [2]
-testNFA = NFA [1,2,3] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1,'a'), [1]), ((1,'b'), [1,2]), ((2,'a'), [2]), ((2,'b'), [2])])  1 [2]
+testNFA = NFA [1,2,3] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1, Just 'a'), [1]), ((1, Just 'b'), [1,2]), ((2, Just 'a'), [2]), ((2,Just 'b'), [2]), ((1, Nothing), [3])])  1 [2]
+
+evaluateDFA :: Eq a => DFA a b -> [b] -> Bool
+evaluateDFA dfa sys = walkDFA (beginDFA dfa) sys `elem` finalDFA dfa where
+    walkDFA state [] = state
+    walkDFA state (s:ss) = walkDFA (transitionDFA dfa (state, s)) ss
+
+evaluateNFA :: Eq a => DFA a b -> [b] -> Bool
+evaluateNFA nfa sys = walkDFA (beginDFA nfa) sys `elem` finalDFA nfa where
+    walkDFA state [] = state
+    walkDFA state (s:ss) = walkDFA (transitionDFA nfa (state, s)) ss
