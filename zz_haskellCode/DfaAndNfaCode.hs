b3fac6171e879de2c9015145058c8688e05867e3
mees <meeslursen@gmail.com>
2024-05-17 10:37:38 +0200

    small fixes

--- zz_haskellCode/DfaAndNfaCode.hs
+++ zz_haskellCode/DfaAndNfaCode.hs
@@ -1,16 +1,15 @@
-module NFA_AND_DFA where
+module DfaAndNfaCode where
 
-data DFA a b =  { statesDFA :: [a]
+data DFA a b    { statesDFA :: [a]
                 , alphabetDFA :: [b]
                 , transitionDFA :: (a,b) -> a
                 , beginDFA :: a
                 , finalDFA :: a
                 }
 
-data NFA a b =  { statesNFA :: [a]
+data NFA a b    { statesNFA :: [a]
                 , alphabetNFA :: [b]
                 , transitionNFA :: (a,b) -> [a]
                 , beginNFA :: a
                 , finalFNA :: a
                 }
-
