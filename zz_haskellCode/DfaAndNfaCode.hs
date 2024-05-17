90751c737c98b6e96c650a21854eede45382c29d
mees <meeslursen@gmail.com>
2024-05-17 15:11:46 +0200

    altered testNFA to have some epsilon transition. Commented out the evaluateNFA function as it does not properly work.

--- zz_haskellCode/DfaAndNfaCode.hs
+++ zz_haskellCode/DfaAndNfaCode.hs
@@ -26,14 +26,16 @@ data NFA state symbol = NFA
 
 
 testDFA = DFA [1,2] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1,'a'), 1), ((1,'b'), 2)])  1 [2]
-testNFA = NFA [1,2,3] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1, Just 'a'), [1]), ((1, Just 'b'), [1,2]), ((2, Just 'a'), [2]), ((2,Just 'b'), [2]), ((1, Nothing), [3])])  1 [2]
+testNFA = NFA [1,2,3] "ab" (\(st,sy) -> fromJust $ lookup (st,sy) [((1, Just 'a'), [1]), ((1, Just 'b'), [1,2]), ((1, Nothing), [3]), ((2, Just 'a'), [2]), ((2,Just 'b'), [2]), ((3, Just 'a'), [2])])  1 [2]
 
 evaluateDFA :: Eq a => DFA a b -> [b] -> Bool
 evaluateDFA dfa sys = walkDFA (beginDFA dfa) sys `elem` finalDFA dfa where
     walkDFA state [] = state
     walkDFA state (s:ss) = walkDFA (transitionDFA dfa (state, s)) ss
 
+{-
 evaluateNFA :: Eq a => DFA a b -> [b] -> Bool
 evaluateNFA nfa sys = walkDFA (beginDFA nfa) sys `elem` finalDFA nfa where
     walkDFA state [] = state
     walkDFA state (s:ss) = walkDFA (transitionDFA nfa (state, s)) ss
+-}
